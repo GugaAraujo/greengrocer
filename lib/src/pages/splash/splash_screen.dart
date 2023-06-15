fe75e463a18ebf0785a1c38b0ad0a1bfa74cef5e
gugaaraujo <gustavoaraujo.dev@gmail.com>
2023-06-14 23:45:44 -0300

    feat: create a splash screen

---
+++ lib/src/pages/splash/splash_screen.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+import 'package:hello_world/src/config/custom_colors.dart';
+import 'package:hello_world/src/pages/commom_widgets/app_name_widget.dart';
+
+import '../auth/sign_in_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2)).then((_) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const SignInScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              CustomColors.customSwatchColor,
+              CustomColors.customSwatchColor.shade700,
+
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            AppNameWidget(
+              greenTitleColor: Colors.white,
+              textSize: 40,
+            ),
+
+            SizedBox(height: 10),
+
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
