6b6ca83bd316c23c671107085e89d3fa27752183
gugaaraujo <gustavoaraujo.dev@gmail.com>
2023-06-16 12:51:48 -0300

    feat: create http service

---
+++ lib/src/services/http_manager.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+
+class HttpMethods {
+  static const get = 'GET';
+  static const post = 'POST';
+  static const put = 'PUT';
+  static const delete = 'DELETE';
+}
+
+class HttpManager {
+  Future<Map> restRequest({
+    required String url,
+    required String method,
+    Map? headers,
+    Map? body,
+  }) async {
+    // Headers da Requisição
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'X-Parse-Application-Id': 'wK7GcEjr2V4br5q5mlR1kybQ5dvxMFDX0qtE1d6Y',
+        'X-Parse-REST-API-Key': '2kahi62fkWePLWAwC7k8aMrtQkobogcgkruMxbeB',
+      });
+
+    Dio dio = Dio();
+
+    try {
+      Response response = await dio.request(
+        url,
+        options: Options(
+          method: method,
+          headers: defaultHeaders,
+        ),
+        data: body,
+      );
+
+      return response.data;
+    } on DioError catch (error) {
+      return error.response?.data ?? {};
+    } catch (error) {
+      return {};
+    }
+  }
+}
